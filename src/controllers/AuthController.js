239dea5f479efe29b631fdde187495a2bdd41856
carvalhaus <ooliveira.joaop@gmail.com>
2024-05-11 15:37:51 -0300

    feat: create authenticate route and generateToken

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth");
+
+const jwtSecret = authConfig.secret;
 
 const UserModel = require("../models/User");
 
 const router = express.Router();
+
+const generateToken = (user = {}) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+    },
+    jwtSecret,
+    {
+      expiresIn: 86400,
+    }
+  );
+};
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
@@ -14,15 +32,38 @@ router.post("/register", async (req, res) => {
     });
   }
 
-  const User = await UserModel.create(req.body);
+  const user = await UserModel.create(req.body);
 
-  User.password = undefined;
+  user.password = undefined;
 
   return res.json({
-    error: false,
-    message: "Registered with success!",
-    data: User,
+    user,
+    token: generateToken(user),
   });
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).json({
+      error: true,
+      message: "User not found!",
+    });
+  }
+
+  if (!(await bcryptjs.compare(password, user.password))) {
+    return res.status(400).send({
+      error: true,
+      message: "Invalid password",
+    });
+  }
+
+  user.password = undefined;
+
+  return res.json({ user, token: generateToken(user) });
 });
 
 module.exports = router;
