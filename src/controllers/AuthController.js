f5db287b1f677ad676dc906b5134e029b61fa710
carvalhaus <ooliveira.joaop@gmail.com>
2024-05-11 10:52:33 -0300

    feat: create AuthController

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const UserModel = require("../models/User");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { email } = req.body;
+
+  if (await UserModel.findOne({ email })) {
+    return res.status(400).json({
+      error: true,
+      message: "User already exists!",
+    });
+  }
+
+  const User = await UserModel.create(req.body);
+
+  User.password = undefined;
+
+  return res.json({
+    error: false,
+    message: "Registered with success!",
+    data: User,
+  });
+});
+
+module.exports = router;
